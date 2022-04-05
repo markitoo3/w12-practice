093749f65fb676d4212cab112a0cb7cf289229f5
markitoo3 <dietrich.marko@gmail.com>
2022-04-05 17:55:20 +0200

    asdasd

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,18 @@ const path = require("path");
 const app = express();
 const port = 9000;
 
+const fFolder = `${__dirname}/../frontend`;
+
+app.use(express.json());
+
+app.use('/pub', express.static(`${fFolder}/public`));
+
 app.get("/", (request, response, next) => {
     response.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+});
+
+app.get("/admin/order-view", (request, response, next) => {
+    response.sendFile(path.join(`${fFolder}/index.html`));
 });
 
 app.get("/kismacska", (request, response, next) => {
@@ -22,29 +32,52 @@ app.get("/api/v1/users", (request, response, next) => {
     console.log("Request received for users endpoint.")
 
     response.sendFile(path.join(`${__dirname}/../frontend/users.json`));
-    
-    /* const users = [
-        {
-            name: "John",
-            surname: "Doe",
-            status: "active"
-        },
-        {
-            name: "Jane",
-            surname: "Scotch",
-            status: "passive"
-        }
-    ]
-    response.send(JSON.stringify(users)) */
 });
 
-app.get("/api/v1/users/active", (request, response, next) => {
+app.get("/api/v1/users-query", (request, response, next) => {
+    /* console.dir(request.query)
+    console.log(request.query.apiKey) */
+    if (request.query.apiKey === "apple") {
+        response.sendFile(path.join(`${__dirname}/../frontend/users.json`))
+    } else {
+        response.send("unauthorized request")
+    }
+    
+});
+
+/* app.get("/api/v1/users-params/:key", (request, response) => {
+    console.dir(request.params);
+    console.log(request.params.key);
+    if (request.params.key === "apple") {
+        response.send("Alma")
+    } else {
+        response.send("Nem alma")
+    }
+    
+}); */
+
+//az alatta lévő active, passive-al megegyező, 1 get requesttel letudva
+
+app.get('/api/v1/users-params/:key',(req, res, next) => {
+    fs.readFile('../frontend/users.json', (error, data) => {
+        const users = JSON.parse(data);
+        if (req.params.key === 'active') {
+            res.send(users.filter(user => user.status === "active"));
+        } else if (req.params.key === 'passive'){
+            res.send(users.filter(user => user.status === "passive"));
+        } else {
+            res.send(`Parameter does not exist `);
+        };
+    });
+});
+
+/* app.get("/api/v1/users/active", (request, response, next) => {
     fs.readFile("../frontend/users.json", (error, data) => {
         if (error) {
             response.send("Error just happened")
         } else {
             const users = JSON.parse(data)
-            /* const activeUsers = users.filter(user => user.status === "active") */
+            // const activeUsers = users.filter(user => user.status === "active")
             response.send(users.filter(user => user.status === "active"))
         }
     })
@@ -57,13 +90,32 @@ app.get("/api/v1/users/passive", (request, response, next) => {
             response.send("Error just happened")
         } else {
             const users = JSON.parse(data)
-            /* const activeUsers = users.filter(user => user.status === "active") */
+            // const activeUsers = users.filter(user => user.status === "active")
             response.send(users.filter(user => user.status === "passive"))
         }
     })
-});
+}); */
 
-app.use('/pub', express.static(`${__dirname}/../frontend/public`));
+app.post("/users/new", (req, res) => {
+    fs.readFile(`${fFolder}/users.json`, (error, data) => {
+        if (error) {
+            console.log(error);
+            res.send("Error reading users file")
+        } else {
+            const users = JSON.parse(data);
+            console.log(req.body);
+            users.push(req.body)
+
+            fs.writeFile(`${fFolder}/users.json`, JSON.stringify(users), error => {
+                if(error) {
+                    console.log(error);
+                    res.send("Error writing users file");
+                }
+            })
+            res.send(req.body)
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`http://127.0.0.1:${port}`);
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -3,16 +3,32 @@ const parseJSON = async (url) => {
     return response.json();
 };
 
-const userComponent = ({name, surname}) => {
+const userComponent = ({firstName, surname}) => {
     return `
-    <div>
-        <h1>${name}</h1>
-        <h2>${surname}</h2>
-    </div>
+        <div>
+            <h1>${firstName}</h1>
+            <h2>${surname}</h2>
+        </div>
     `;
 };
 
+function addUserComponent() {
+    return `
+        <div>
+            <input class="firstName" type="text" name="First name" placeholder="First Name">
+            <input class="surname" type="text" name="SurName" placeholder="Surname">
+            <button class="btn">Send</button>
+        </div>
+    `;
+}
+
 const loadEvent = async () => {
+
+    if (window.location.pathname === '/admin/order-view'){
+        console.log("mi most az admin felületen vagyunk");
+    } else {
+        console.log("mi most a vásárlói felületen vagyunk");
+    }
 
     const result = await parseJSON("/api/v1/users");
     const rootElement = document.getElementById("root");
@@ -20,6 +36,35 @@ const loadEvent = async () => {
         "beforeend",
         result.map(user => userComponent(user)).join("")
     );
+
+    rootElement.insertAdjacentHTML("afterend", addUserComponent());
+
+
+    const button = document.querySelector('.btn')
+    const firstName = document.querySelector('.firstName')
+    const surname = document.querySelector('.surname')
+
+    button.addEventListener("click", e => {
+        const userData = {
+            firstName: firstName.value,
+            surname: surname.value
+        };
+
+        fetch("/users/new", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        })
+        .then(async data => {
+            const user = await data.json();
+
+            rootElement.insertAdjacentHTML("beforeend", userComponent(user))
+        })
+ 
+    })
+
 };
 
 window.addEventListener("load", loadEvent)
